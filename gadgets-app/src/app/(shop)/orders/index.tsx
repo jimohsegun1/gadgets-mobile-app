23e23807df368925e6728e646a8d91290d65e123
jimohsegun1 <jimohsegunj@gmail.com>
2025-04-04 17:28:05 +0100

    display orders in app

--- gadgets-app/src/app/(shop)/orders/index.tsx
+++ gadgets-app/src/app/(shop)/orders/index.tsx
@@ -1,39 +1,33 @@
 import {
+  ActivityIndicator,
   FlatList,
   ListRenderItem,
   Pressable,
   StyleSheet,
   Text,
   View,
-} from "react-native";
-import React from "react";
-import { Link, Stack } from "expo-router";
-import { ORDERS } from "../../../../assets/orders";
-import { Order, OrderStatus } from "../../../../assets/types/order";
+} from 'react-native';
+import { Link, Stack } from 'expo-router';
+import { format } from 'date-fns';
 
-const statusDisplayText: Record<OrderStatus, string> = {
-  Pending: "Pending",
-  Completed: "Completed",
-  Shipped: "Shipped",
-  InTransit: "In Transit",
-};
+import { Tables } from '../../../types/database.types';
+import { getMyOrders } from '../../../api/api';
 
-const renderItem: ListRenderItem<Order> = ({ item }) => (
+const renderItem: ListRenderItem<Tables<'order'>> = ({ item }) => (
   <Link href={`/orders/${item.slug}`} asChild>
     <Pressable style={styles.orderContainer}>
       <View style={styles.orderContent}>
         <View style={styles.orderDetailsContainer}>
-          <Text style={styles.orderItem}>{item.item}</Text>
-          <Text style={styles.orderDetails}>{item.details}</Text>
-          <Text style={styles.orderDate}>{item.date}</Text>
+          <Text style={styles.orderItem}>{item.slug}</Text>
+          <Text style={styles.orderDetails}>{item.description}</Text>
+          <Text style={styles.orderDate}>
+            {format(new Date(item.created_at), 'MMM dd, yyyy')}
+          </Text>
         </View>
-
         <View
           style={[styles.statusBadge, styles[`statusBadge_${item.status}`]]}
         >
-          <Text style={styles.statusText}>
-            {statusDisplayText[item.status]}
-          </Text>
+          <Text style={styles.statusText}>{item.status.toUpperCase()}</Text>
         </View>
       </View>
     </Pressable>
@@ -41,12 +35,32 @@ const renderItem: ListRenderItem<Order> = ({ item }) => (
 );
 
 const Orders = () => {
+  const { data: orders, error, isLoading } = getMyOrders();
+
+  if (isLoading) return <ActivityIndicator />;
+
+  if (error || !orders) return <Text>Error: {error?.message}</Text>;
+
+  if (!orders.length)
+    return (
+      <Text
+        style={{
+          fontSize: 16,
+          color: '#555',
+          textAlign: 'center',
+          padding: 10,
+        }}
+      >
+        No orders created yet
+      </Text>
+    );
+
   return (
     <View style={styles.container}>
-      <Stack.Screen options={{ title: "Orders" }} />
+      <Stack.Screen options={{ title: 'Orders' }} />
       <FlatList
-        data={ORDERS}
-        keyExtractor={(item) => item.id.toString()}
+        data={orders}
+        keyExtractor={item => item.id.toString()}
         renderItem={renderItem}
       />
     </View>
@@ -55,59 +69,59 @@ const Orders = () => {
 
 export default Orders;
 
-const styles = StyleSheet.create({
+const styles: { [key: string]: any } = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
   },
   orderContainer: {
-    backgroundColor: "#f8f8f8",
+    backgroundColor: '#f8f8f8',
     padding: 16,
     marginVertical: 8,
     borderRadius: 8,
   },
   orderContent: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
   orderDetailsContainer: {
     flex: 1,
   },
   orderItem: {
     fontSize: 18,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   orderDetails: {
     fontSize: 14,
-    color: "#555",
+    color: '#555',
   },
   orderDate: {
     fontSize: 12,
-    color: "#888",
+    color: '#888',
     marginTop: 4,
   },
   statusBadge: {
     paddingVertical: 4,
     paddingHorizontal: 8,
     borderRadius: 4,
-    alignSelf: "flex-start",
+    alignSelf: 'flex-start',
   },
   statusText: {
     fontSize: 12,
-    fontWeight: "bold",
-    color: "#fff",
+    fontWeight: 'bold',
+    color: '#fff',
   },
   statusBadge_Pending: {
-    backgroundColor: "#ffcc00",
+    backgroundColor: '#ffcc00',
   },
   statusBadge_Completed: {
-    backgroundColor: "#4caf50",
+    backgroundColor: '#4caf50',
   },
   statusBadge_Shipped: {
-    backgroundColor: "#2196f3",
+    backgroundColor: '#2196f3',
   },
   statusBadge_InTransit: {
-    backgroundColor: "#ff9800",
+    backgroundColor: '#ff9800',
   },
 });
